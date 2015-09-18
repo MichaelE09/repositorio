34a7a6ec8ee50a40d9d425f3d28decbc14a4cd2d
Michael <michaelericles2013@hotmail.com>
2015-09-17 21:09:22 -0300

    criando soma

---
+++ calculadora.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>calculadora</title>
+</head>
+
+<body>
+<form action="calculora.php"method="get">
+
+<input name = "num1" type="text"/>
+<br/> <br />
+
+<input name="name2" type="text"/>
+<br /> <br/>
+
+
+<input name="oporecao" type="text" />
+<br/> <br/>
+
+<input type ="submit" value="submeter" />
+<br/> <br/>
+
+</form>  
+</body>
+</html>
